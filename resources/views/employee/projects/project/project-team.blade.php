9b6f01dc76b3500d67e8d112af340c3ccd658bfe
xilujain <lujainboo@gmail.com>
2024-12-02 11:57:48 +0300

    project team ui solved

--- resources/views/employee/projects/project/project-team.blade.php
+++ resources/views/employee/projects/project/project-team.blade.php
@@ -2,11 +2,11 @@
     <h1 class="font-bold text-xl">فريق العمل</h1>
     <div class="grid grid-cols-2 gap-x-[3.3rem] my-8 font-normal">
         <div class="grid gap-y-5">
-            <label>مدير المشروع</label>
+            <label class="text-base">مدير المشروع</label>
             <input class="input" type="text" disabled placeholder="{{ $bigBoss->project_manager ?? '' }}" />
         </div>
         <div class="grid gap-y-5">
-            <label>نائب مدير المشروع</label>
+            <label class="text-base">نائب مدير المشروع</label>
             <input class="input" type="text" disabled placeholder="{{ $bigBoss->sub_project_manager ?? '' }}" />
         </div>
     </div>
@@ -17,11 +17,11 @@
             @foreach($team as $member)
             @if($member['role'] !== null)
             <div class="grid gap-y-5 my-4">
-                <label>اسم العضو</label>
+                <label class="text-base">اسم العضو</label>
                 <input class="input" type="text" disabled placeholder="{{ $member['name'] ?? '' }}" />
             </div>
             <div class="grid gap-y-5">
-                <label>الدور</label>
+                <label class="text-base">الدور</label>
                 <input class="input" type="text" disabled placeholder="{{ $member['role'] ?? '' }}" />
             </div>
             @endif
